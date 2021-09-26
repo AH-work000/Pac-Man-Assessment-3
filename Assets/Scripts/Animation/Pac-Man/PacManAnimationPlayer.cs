a51347ac9a66521cfa51bf78a67773175d324e56
AH-work000 <70098254+AH-work000@users.noreply.github.com>
2021-09-27 02:22:06 +1000

    FAmin-1.6

--- Assets/Scripts/Animation/Pac-Man/PacManAnimationPlayer.cs
+++ Assets/Scripts/Animation/Pac-Man/PacManAnimationPlayer.cs
@@ -6,80 +6,146 @@ public class PacManAnimationPlayer : MonoBehaviour
 {
     // Fields
     public Animator animController;
-    private bool notAtRightPosition;
-    private bool notAtDownPosition;
-    private bool notAtUpPosition;
-    private bool notAtLeftPosition;
+
+    /* Input testing code IGNORE:
+            private bool notAtRightPosition;
+            private bool notAtDownPosition;
+            private bool notAtUpPosition;
+            private bool notAtLeftPosition;
+    */
 
     // Start is called before the first frame update
     void Start()
     {
-        // Initialize the position bools
-        notAtRightPosition = true;
-        notAtDownPosition = true;
-        notAtUpPosition = true;
-        notAtLeftPosition = true;
+        /* Input testing code IGNORE:
+                // Initialize the position bools
+                notAtRightPosition = true;
+                notAtDownPosition = true;
+                notAtUpPosition = true;
+                notAtLeftPosition = true;
+        */
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        // If the "D" Key or the Right Arrow Key is pressed then Pac-Man shifts to its Right Animation. 
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            if (notAtRightPosition)
-            {
-                animController.SetTrigger("SwitchRightAmin");
-                notAtRightPosition = false;
-                notAtDownPosition = true;
-                notAtUpPosition = true;
-                notAtLeftPosition = true;
-            }
+        /* Input testing code IGNORE: 
+                // If the "D" Key or the Right Arrow Key is pressed then Pac-Man shifts to its Right Animation. 
+                if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    if (notAtRightPosition)
+                    {
+                        animController.SetTrigger("SwitchRightAnim");
+                        notAtRightPosition = false;
+                        notAtDownPosition = true;
+                        notAtUpPosition = true;
+                        notAtLeftPosition = true;
+                    }
 
-        }
+                }
 
-        // If the "S" Key or the Down Arrow Key is pressed then Pac-Man shifts to its Downwards Animation.
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            if(notAtDownPosition)
-            {
-                animController.SetTrigger("SwitchDownAmin");
-                notAtDownPosition = false;
-                notAtRightPosition = true;
-                notAtUpPosition = true;
-                notAtLeftPosition = true;
-            }
-        }
+                // If the "S" Key or the Down Arrow Key is pressed then Pac-Man shifts to its Downwards Animation.
+                if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    if(notAtDownPosition)
+                    {
+                        animController.SetTrigger("SwitchDownAnim");
+                        notAtDownPosition = false;
+                        notAtRightPosition = true;
+                        notAtUpPosition = true;
+                        notAtLeftPosition = true;
+                    }
+                }
 
-        // If the "W" Key or the Up Arrow Key is pressed then Pac-Man shifts to its Upward Animation.
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            if (notAtUpPosition)
-            {
-                animController.SetTrigger("SwitchUpAmin");
-                notAtUpPosition = false;
-                notAtRightPosition = true;
-                notAtDownPosition = true;
-                notAtLeftPosition = true;
-            }
-        }
+                // If the "W" Key or the Up Arrow Key is pressed then Pac-Man shifts to its Upward Animation.
+                if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+                {
+                    if (notAtUpPosition)
+                    {
+                        animController.SetTrigger("SwitchUpAnim");
+                        notAtUpPosition = false;
+                        notAtRightPosition = true;
+                        notAtDownPosition = true;
+                        notAtLeftPosition = true;
+                    }
+                }
 
-        // If the "A" Key or the Left Arrow Key is pressed then Pac-Man shifts to its Left Animation.
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            if(notAtLeftPosition)
-            {
-                animController.SetTrigger("SwitchLeftAmin");
-                notAtLeftPosition = false;
-                notAtUpPosition = true;
-                notAtRightPosition = true;
-                notAtDownPosition = true;
-            }
-        }
+                // If the "A" Key or the Left Arrow Key is pressed then Pac-Man shifts to its Left Animation.
+                if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+                {
+                    if(notAtLeftPosition)
+                    {
+                        animController.SetTrigger("SwitchLeftAnim");
+                        notAtLeftPosition = false;
+                        notAtUpPosition = true;
+                        notAtRightPosition = true;
+                        notAtDownPosition = true;
+                    }
+                }
 
-        // If the "K" Key is pressed then Pac-Man shifts to play its death Animation.
-        if (Input.GetKeyDown(KeyCode.K))
-            animController.SetTrigger("SwitchDeadAmin");
+                // If the "K" Key is pressed then Pac-Man shifts to play its death Animation.
+                if (Input.GetKeyDown(KeyCode.K))
+                    animController.SetTrigger("SwitchDeadAmin");
+
+        */
+
+
+        // Below code to automatically play the animation
+
+            // Play the Right-Movement Animation
+            InvokeRepeating("playRightAnim", 0f, 18f);
+
+            // Play the Down-Movement Animation
+            InvokeRepeating("playDownAnim", 3f, 18f);
+
+            // Play the Upwards-Movement Animation
+            InvokeRepeating("playUpAnim", 6f, 18f);
+
+            // Play the Left-Movement Animation
+            InvokeRepeating("playLeftAnim", 9f, 18f);
+
+            // Play the Death Animation
+            InvokeRepeating("playDeathAnim", 12f, 18f);
+
+            // Exit out of the Animation Cycle
+            InvokeRepeating("playExit", 15f, 18f);
+    }
+
+    // Helper Methods
+    private void playRightAnim()
+    {
+        animController.SetTrigger("SwitchRightAnim");
+    }
+
+
+    private void playDownAnim()
+    {
+        animController.ResetTrigger("SwitchRightAnim");
+        animController.SetTrigger("SwitchDownAnim");
+    }
+
+    private void playUpAnim()
+    {
+        animController.ResetTrigger("SwitchDownAnim");
+        animController.SetTrigger("SwitchUpAnim");
+    }
+
+    private void playLeftAnim()
+    {
+        animController.ResetTrigger("SwitchUpAnim");
+        animController.SetTrigger("SwitchLeftAnim");
+    }
+
+    private void playDeathAnim()
+    {
+        animController.ResetTrigger("SwitchLeftAnim");
+        animController.SetTrigger("SwitchDeadAnim");
+    }
+
+    private void playExit()
+    {
+        animController.ResetTrigger("SwitchDeadAnim");
+        animController.SetTrigger("Exit");
     }
 }
