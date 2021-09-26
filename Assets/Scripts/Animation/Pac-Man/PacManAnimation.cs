f78720d0f0b2539e3ed32ee4de42f45b262ec138
AH-work000 <70098254+AH-work000@users.noreply.github.com>
2021-09-26 20:33:03 +1000

    FAmin-1.2

--- Assets/Scripts/Animation/Pac-Man/PacManAnimation.cs
+++ Assets/Scripts/Animation/Pac-Man/PacManAnimation.cs
@@ -6,19 +6,43 @@ public class PacManAnimation : MonoBehaviour
 {
     // Fields
     public Animator animationController;
+    private bool notAtRightPosition;
+    private bool notAtDownPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Initialize the position bools
+        notAtRightPosition = true;
+        notAtDownPosition = true;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // If the "D" Key or the Right Arrow Key is pressed then Pac-Man shifts to its Right Animation. 
         if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
-            animationController.SetTrigger("RotateRightParam");
+            if (notAtRightPosition)
+            {
+                animationController.SetTrigger("SwitchRightAmin");
+                notAtDownPosition = true;
+                notAtRightPosition = false;
+            }
+
         }
+
+        // If the "S" Key or the Downm Arrow Key is pressed then Pac-Man shifts to its Downwards Animation.
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if(notAtDownPosition)
+            {
+                animationController.SetTrigger("SwitchDownwardsAmin");
+                notAtRightPosition = true;
+                notAtDownPosition = false;
+            }
+        }
+
     }
 }
