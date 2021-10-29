63ac937cd7d534a1153b5388c354d6ea314ca9ce
AH-work000 <70098254+AH-work000@users.noreply.github.com>
2021-10-29 23:58:52 +1100

    CatchUpAssgn3-1.7

--- Assets/Scripts/C#-Code-Currently-Used/Animation/Pac-Man/PacManAnimationCycle.cs
+++ Assets/Scripts/C#-Code-Currently-Used/Animation/Pac-Man/PacManAnimationCycle.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class PacManAnimationCycle : MonoBehaviour
 {
+    // Fields -- Normal
+    public Animator playerAnimator;
+    bool isAnimCycleNotEnabled;
+
+    // Fields -- Coroutine
+    Coroutine currentCoroutine;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isAnimCycleNotEnabled = true;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (currentCoroutine == null && isAnimCycleNotEnabled)
+        {
+            currentCoroutine = StartCoroutine(animationCycle());
+            isAnimCycleNotEnabled = false;
+        }
+        else
+        {
+            currentCoroutine = null;
+        }
+             
     }
+
+    IEnumerator animationCycle()
+    {
+
+        playerAnimator.SetTrigger("Left");
+        yield return new WaitForSeconds(3.0f);
+        playerAnimator.ResetTrigger("Left");
+        playerAnimator.SetTrigger("Right");
+        yield return new WaitForSeconds(3.0f);
+        playerAnimator.ResetTrigger("Right");
+        playerAnimator.SetTrigger("Up");
+        yield return new WaitForSeconds(3.0f);
+        playerAnimator.ResetTrigger("Up");
+        playerAnimator.SetTrigger("Down");
+        yield return new WaitForSeconds(3.0f);
+        playerAnimator.ResetTrigger("Down");
+        playerAnimator.SetTrigger("Death");
+        yield return new WaitForSeconds(8.0f);
+        playerAnimator.ResetTrigger("Death");
+        playerAnimator.SetTrigger("endDeath");
+
+        // Set isAnimCucleEnabled to be true at the end of the cycle
+        isAnimCycleNotEnabled = true; 
+    }
+
 }
